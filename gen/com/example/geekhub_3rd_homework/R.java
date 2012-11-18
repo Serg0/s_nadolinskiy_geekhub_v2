22dd369487128d88bef6e09c80415b54700ad796
Serg0 <sernad@mail.ru>
2012-11-18 05:15:24 +0200

    vN+4 partial rotation support

--- gen/com/example/geekhub_3rd_homework/R.java
+++ gen/com/example/geekhub_3rd_homework/R.java
@@ -12,8 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon_arrow=0x7f020002;
+        public static final int ic_arrow_selected=0x7f020001;
+        public static final int ic_arrow_unselected=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon_arrow=0x7f020004;
     }
     public static final class id {
         public static final int WebView1=0x7f070006;
--- src/com/example/geekhub_3rd_homework/DetailsFragment.java
+++ src/com/example/geekhub_3rd_homework/DetailsFragment.java
@@ -6,17 +6,21 @@ import java.net.URLEncoder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class DetailsFragment extends Fragment {
-	String content2;
-	String content;
+//	String content2;
+//	String content;
 	int contentPos;
-
+	
+	final String LOG_TAG = "myLogs";
+	
 	public DetailsFragment() {
 		// TODO Auto-generated constructor stub
 	}
@@ -28,22 +32,61 @@ public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	return inflater.inflate(R.layout.fragment_details, null);
 }
 
+@Override
+public void onSaveInstanceState(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onSaveInstanceState(savedInstanceState);
+	savedInstanceState.putInt("contentPos", contentPos);
+	System.out.println("contentPos in saveInstance" + contentPos);
+}
 
 @SuppressWarnings("deprecation")
 @Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
-//       if(savedInstanceState != null){
-//    	   content = savedInstanceState.getString("content");
-//       }
-//    	   
-//	  Intent intent = this.getActivity().getIntent();
-//	    content = intent.getStringExtra("content");
-//	    
-	    Intent intent = this.getActivity().getIntent();
-	    contentPos = intent.getIntExtra("contentPos", -1);
+		Log.d(LOG_TAG, "befor Destroy");
+		 if (savedInstanceState != null){
+			    contentPos = savedInstanceState.getInt("contentPos");
+			    Log.d(LOG_TAG, "onSaveInstanceState"+contentPos);
+			    if (MainActivity.isTablet(getActivity())&&(!MainActivity.isLandscape(getActivity()))){
+			    Intent intent = new Intent(this.getActivity(), SecondActivity.class); 
+        		intent.putExtra("contentPos", contentPos);
+        		startActivity(intent);
+//        		try {
+//        			getActivity().finish();
+//        			
+//					Log.d(LOG_TAG, "fINALIZED");
+//					return;
+//				} catch (Throwable e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+        		return;
+			    }else 
+			    	if (MainActivity.isTablet(getActivity())&&(MainActivity.isLandscape(getActivity()))){
+//			    	try {
+//	        			getActivity().finish();
+//	        			
+//						Log.d(LOG_TAG, "fINALIZED");
+//						return;
+//					} catch (Throwable e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+			    	
+        		//this.getActivity().getFragmentManager().getFragment(getArguments(), getTag());
+			    
+    }
+			   
+			  }else{
+				  Log.d(LOG_TAG, "NON SAVED INSTANCE");
+				  Intent intent = this.getActivity().getIntent();
+				    contentPos = intent.getIntExtra("contentPos", -1);
+			  }
+
 	    
+		 Log.d(LOG_TAG, "after Destroy");
 	    WebView webview = (WebView) getActivity().findViewById(R.id.WebView1);
     String query = null;
 		try {
